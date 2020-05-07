2561458e0255e865b9625d3b29cb01d9ee42f261
qyhsmxe <1069542931@qq.com>
2020-05-07 14:30:20 +0800

    YanghuiTriangle

--- src/array_string/summary/ArrayRotate.java
+++ src/array_string/summary/ArrayRotate.java
@@ -1,9 +1,88 @@
 package array_string.summary;
 
+import java.util.Arrays;
+
 /**
- * @description: 这是代码里唯一的注释
- * @author: QinYong
- * @create: 2020-05-07 09:09
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
  **/
 public class ArrayRotate {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        test(nums,k);
+    }
+
+    /**
+     * 暴力解法
+     * 1 2 3 4 5 6 7
+     * 向右旋转 3
+     * 5 6 7 1 2 3 4
+     */
+    private static void test1(int[] nums,int k){
+        for (int i = 0; i < k; i++) {
+            int previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                int tmp = nums[j];
+                nums[j] = previous;
+                previous = tmp;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * 额外数组
+     * 新数组位置 i+k % len
+     */
+    private static void test2(int[] nums,int k){
+        int len = nums.length;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[(i+k)%len] = nums[i];
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+    /**
+     * 二级反转
+     * 新数组位置 i+k % len
+     */
+    private static void test(int[] nums,int k){
+        int len = nums.length;
+        k %= len;
+        // reverse the whole array
+        reverse(nums,0,len-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,len-1);
+        System.out.println(Arrays.toString(nums));
+    }
+    private static void reverse(int[] nums,int l,int r){
+        while (l < r){
+            int tmp = nums[l];
+            nums[l++] = nums[r];
+            nums[r--] = tmp;
+        }
+    }
+
+    /**
+     * 使用环状替换
+     */
+    private static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+
 }
--- src/array_string/summary/YanghuiTriangle.java
+++ src/array_string/summary/YanghuiTriangle.java
@@ -1,9 +1,37 @@
 package array_string.summary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @description: 这是代码里唯一的注释
  * @author: QinYong
  * @create: 2020-05-07 11:29
  **/
 public class YanghuiTriangle {
+    public static void main(String[] args) {
+        System.out.println(getRow(3));
+    }
+    public static List<Integer> getRow(int rowIndex) {
+        if(rowIndex < 0) return new ArrayList<>();
+        if(rowIndex == 0) return new ArrayList<>(Collections.singletonList(1));
+        if(rowIndex == 1) return new ArrayList<>(Arrays.asList(1,1));
+        int[] res = new int[rowIndex + 1];
+        res[0] = 1;
+        res[1] = 1;
+        for(int i = 1; i < rowIndex; i++) {
+            for(int j = i; j > 0; j--) {
+                res[j] = res[j] + res[j - 1];
+            }
+            res[i + 1] = 1;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int n : res) {
+            ans.add(n);
+        }
+        return ans;
+    }
+
 }
